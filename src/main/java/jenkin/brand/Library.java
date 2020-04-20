20e6f7471be560a26d6b222559a8e518c938877a
thangqt <thang.quoctruong1204@gmail.com>
2020-04-19 21:45:35 -0700

    first test 3

--- src/main/java/jenkin/brand/Library.java
+++ src/main/java/jenkin/brand/Library.java
@@ -6,6 +6,7 @@ package jenkin.brand;
 public class Library {
     public boolean someLibraryMethod() {
       System.out.println("Test branch");
+      System.out.println("Test 333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333");
         return true;
     }
 }
